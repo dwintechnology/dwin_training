2f19ad100502e1365727260cfdc51b062e08b069
Sasun92D <sasun.davtyan.dwin@gmail.com>
2021-12-22 11:44:27 +0400

    changes

--- sasun_davtyan/JavaScript/21.12.2021/task5.js
+++ sasun_davtyan/JavaScript/21.12.2021/task5.js
@@ -1,11 +1,7 @@
 let num =6;
-var total = 1;
+let total = 1;
 
 for (let i = 0; i < num; i++){
-    console.log('i=',i)
     total = total * (num - i);
-
-
-console.log(num + '! = ' + total);
 }
 console.log(total)
