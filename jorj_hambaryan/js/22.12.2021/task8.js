f8d86cacbf97457393e26abece8b2cd149882a58
Jorj99 <jorj.hambaryan.dwin@gmail.com>
2021-12-23 12:50:43 +0400

    22 12 2021

--- jorj_hambaryan/js/22.12.2021/task8.js
+++ jorj_hambaryan/js/22.12.2021/task8.js
@@ -2,30 +2,15 @@
 // Write a function that returns the 0's count at the end of the number for the factorial of the given number FOR BIG NUMBERS(e.g. 10000).
 // e.g. 8! = 40320. The function should return 1
 
+let a = prompt('','');
 
-let rev = 0;
-let num = prompt('','');
-let num2;
-let jor = 0;
-let f = 1;
-
-
-while(num != 0){
-	num2 = num % 10;
-  rev = rev * 10 + num2;
-  if(num2 == 0 ){
-      jor++;
-  }
-  num = Math.floor(num/10);
-  
-
-}
-
-function factorial(){
-    for (let index = 1; index <= jor; index++) {
-        f = index * f;
+function factoial0 (n){
+   let j = 0;
+    for (let index = 1; n >= Math.pow(5,index); index++) {
+       let result = n / Math.pow(5,index);
+       result = Math.floor(result);
+       j = result + j;
     }
-    return f;
+    alert(j);
 }
-factorial();
-console.log(f);
+factoial0(a);
