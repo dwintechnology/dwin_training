5b2d986f839dcd06f825e5b97a95ffbc7a54d17e
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-01-10 12:49:06 +0400

    homework

--- Arman_Khoyetsyan/homework 04.01.22.js
+++ Arman_Khoyetsyan/homework 04.01.22.js
@@ -34,4 +34,74 @@
 // }
 // randomName(studentName)
 
+// task 3 Credit Card Validation
 
+let userCardNumber = prompt("Write your card number 16 pieces")
+let userName = prompt("Write your name")
+let userCVC = +prompt("Write your CVC")
+let cardData = prompt("Write your card data")
+
+let obj = {
+    Name:userName,
+    Number: userCardNumber,
+    CVC:userCVC,
+    Data:cardData,
+    valid: true,
+}
+function stringToNumber(str) {
+    if (str.length === 16 || str.length === 19) {
+        if (str.length === 16) {
+            return numberCheck(str)
+        } else if (str.length === 19) {
+            return characterFiltering(str)
+        }
+    } else {
+        return alert("Error");
+    }
+
+}
+function numberCheck(str) {
+    let arr = str.split("")
+    for (let i = 0; i < arr.length; i++) {
+        if (isNaN(arr[i])) {
+            return false
+        }
+    }
+    return isTheLasNumberAEven(str)
+}
+function characterFiltering(str) {
+    let arr = str.split("-")
+    let newStr = arr[0] + arr[1] + arr[2] + arr[3]
+    return numberCheck(newStr)
+
+}
+function isTheLasNumberAEven(str) {
+    let arr = str.split("")
+    if (arr[arr.length - 1] % 2 === 0) {
+        return AreAllNumbersTheSame(str)
+    }
+    
+    return false
+}
+
+function AreAllNumbersTheSame(str) {
+    let arr = str.split("")
+    let num = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== num) {
+            return true
+        }
+
+    }
+    
+    return false
+}
+
+
+if (stringToNumber(userCardNumber)) {
+    console.log(obj)
+} else {
+    obj.valid = false;
+    obj.error = "error";
+    console.log(obj)
+}
