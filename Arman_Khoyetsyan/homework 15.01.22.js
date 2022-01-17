df3bc67baadf41daeac65750ea94680014761fb9
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-01-17 14:24:17 +0400

    Homework Promise

--- Arman_Khoyetsyan/homework 15.01.22.js
+++ Arman_Khoyetsyan/homework 15.01.22.js
@@ -1,55 +1,51 @@
 // Task 1 
 
-function atLeastThree(arr, cb) {
-    let oddNumber = 0
-    arr.forEach((num) => {
-        if (cb(num)) {
-            oddNumber++
-        }
-    });
-    return (oddNumber >= 3) 
+// function atLeastThree(arr, cb) {
+//     let oddNumber = 0
+//     arr.forEach((num) => {
+//         if (cb(num)) {
+//             oddNumber++
+//         }
+//     });
+//     return (oddNumber >= 3) 
 
     
 
-}
+// }
 
-function isOdd(number) {
-    if (number % 2 !== 0) return true
+// function isOdd(number) {
+//     if (number % 2 !== 0) return true
 
 
-}
-console.log(atLeastThree([2, 6,11, 7, 9, 6], isOdd))
+// }
+// console.log(atLeastThree([2, 6,11, 7, 9, 6], isOdd))
 
 // task 1 2 version
 
-// function atLeastThree2(arr) {
+ function atLeastThree2(arr) {
 
-//     let promis = new Promise(function (resolve, reject) {
-//         let resolveSum = 0
-//         arr.forEach(num => {
-//             if (num % 2 === 0) {
-//                 resolveSum++
-//             }
-//         });
+    let promis = new Promise(function (resolve, reject) {
+        let resolveSum = 0
+        arr.forEach(num => {
+            if (num % 2 !== 0) {
+                resolveSum++
+            }
+        });
 
-//         if (resolveSum >= 3) {
-//             resolve(true)
-//         }
-//         reject("Error")
+        if (resolveSum >= 3) {
+            resolve(true)
+        }
+        reject("Error")
 
-//     })
+    })
 
-//     promis.then(function (resolve) {
-//         console.log(resolve)
-//     })
+    promis.then(console.log)
 
-//     promis.catch(function (reject) {
-//         console.log(reject)
-//     })
+    promis.catch(console.log)
 
-// }
+ }
 
-// atLeastThree2([2,4,6,7,5,9])
+atLeastThree2([2,4,6,4,5,9])
 
 //Task 2 
 
