0d4c7ff1282cd0998f3b1f45820aa356e4f6b105
Sasun92D <sasun.davtyan.dwin@gmail.com>
2022-01-18 02:53:11 +0400

    homework & classwork

--- sasun_davtyan/JavaScript/15.01.2022/task2.js
+++ sasun_davtyan/JavaScript/15.01.2022/task2.js
@@ -12,3 +12,21 @@ function itsCallBack(str) {
     return str;
 }
 repeatFunc('abc', 2, itsCallBack)
+
+//promise
+
+function repeatFunc(str, number){
+    while(number>0){
+     let possible = "ABCDEdclckseKFMSEFKSEWKFJSEKFOPSWEJFPOEWIFGHIJKLMNOPQRSTUVWXYZ";
+    str = str + " " + possible.charAt(Math.floor(Math.random() * possible.length));
+    number--;
+}  
+return new Promise((resolve,reject)=>{
+    resolve(str)
+});
+    }
+
+let x = repeatFunc("abc",5);
+x.then((data)=>{
+    console.log(data)
+})
