6713795d2694713a713f95bb7b59cb34055f25cd
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-01-26 11:58:08 +0400

    Homework and classwork

---
+++ Arman_Khoyetsyan/classwork 24.01.22.js
@@ -0,0 +1,25 @@
+class myStack {
+    constructor() {
+        let arr = []
+        this.arr = arr
+    }
+    myPush(a) {
+        this.arr.push(a)
+    }
+    myPeekBack() {
+        return this.arr[this.arr.length - 1]
+    }
+    myPop() {
+        return this.arr.pop()
+
+    }
+
+
+
+}
+
+
+let a = new myStack
+
+
+a.myPush(5)
