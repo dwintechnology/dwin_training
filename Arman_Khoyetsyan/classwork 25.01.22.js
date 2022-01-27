d7e50a3fb8e0afa435bb7c94bf6faa1cdddc3080
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-01-27 12:36:17 +0400

    Homework

--- Arman_Khoyetsyan/classwork 25.01.22.js
+++ Arman_Khoyetsyan/classwork 25.01.22.js
@@ -5,6 +5,7 @@
 //         this.value = value
 //     }
 // }
+
 // class Tree {
 //     constructor() {
 //         this.root = null;
@@ -39,15 +40,71 @@
 //         recPush(this.root)
 
 //     }
-//     nodeDel(val) {
+//     nodeDel(value) {
+//         if (this.root == null) {
+//             return null;
+//         }
 
+//         let rightBranch;
+
+//         function foo(current) {
+
+//             if (value == current.value) {
+//                 rightBranch = current.right;
+//                 if (current.left !== null) {
+//                     current.value = current.left.value;
+//                 } else {
+//                     if (current.right !== null) {
+//                         current.value = current.right.value;
+//                         current.right = current.right.right;
+//                         return
+//                     } else {
+//                         delete current.left;
+//                         delete current.right;
+//                         delete current.value
+
+//                         return
+//                     }
+
+//                 }
+
+//                 if (current.right !== null) {
+//                     current.right = current.left.right;
+//                 }
+//                 if (current.left !== null) {
+//                     current.left = current.left.left;
+//                 }
+
+//                 function foo2(current) {
+//                     if (current.right == null) {
+//                         current.right = rightBranch
+//                         return
+//                     }
+//                     return foo2(current.right)
+//                 }
+//                 return foo2(current)
+//             }
+//             if (value < current.value) {
+//                 foo(current.left)
+//             }
+//             if (value > current.value) {
+//                 foo(current.right)
+//             }
+//         }
+//         return foo(this.root)
 //     }
 // }
 
 // let tree = new Tree()
-// tree.insert(20)
+// tree.insert(100)
 // tree.insert(9)
 // tree.insert(8)
 // tree.insert(50)
 // tree.insert(86)
 // tree.insert(21)
+// tree.insert(150)
+// tree.insert(140)
+// tree.insert(145)
+// tree.insert(160)
+// tree.insert(155)
+// tree.nodeDel(9)
--- Arman_Khoyetsyan/homework 24.01.22.js
+++ Arman_Khoyetsyan/homework 24.01.22.js
@@ -1,76 +1,182 @@
-// function createNode(value) {
-//     return {
-//         value: value,
-//         next: null,
-//     };
-// }
+class CreateNode {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
 
-// class LinkedList {
-//     constructor() {
-//         this.length = 0
-//         this.start = null
+class LinkedList {
+    constructor() {
+        this.length = 0
+        this.start = null
 
-//     }
+    }
 
-//     insert(val) {
-//         this.length++
-//             let node = createNode(val)
-//         if (this.start === null) {
-//             this.start = node
-//             return node
-//         }
-//         let current = this.start
-//         while (current.next) {
-//             current = current.next
-//         }
-//         current.next = node
-//         return node
-//     }
+    insert(val) {
+        this.length++
+            let node = new CreateNode(val)
+        if (this.start === null) {
+            this.start = node
+            return node
+        }
+        let current = this.start
+        while (current.next) {
+            current = current.next
+        }
+        current.next = node
+        return node
+    }
 
-//     insertPosition(val, position) {
-//         let previousNode = null;
-//         let current = this.start
+    insertPosition(val, position) {
+        let previousNode = null;
+        let prevNode = null;
+        let current = this.start
 
-//         for (let i = 0; i < position; i++) {
-//             previousNode = current
-//             current = current.next
+        for (let i = 0; i < position; i++) {
+            if (i > 0) {
+                prevNode = previousNode
+            }
+            previousNode = current
+            current = current.next
 
-//         }
-//         if (position === 0) {
-//             let node = createNode(val)
-//             this.length++;
-//             node.next = current
-//             this.start = node
+        }
+        if (position === 0) {
+            let node = new CreateNode(val)
+            this.length++;
+            node.next = current
+            this.start = node
 
-//             return node
-//         }
-//         let node = createNode(val)
-//         this.length++;
-//         node.next = current
-//         current = node
-//         return node
-//     }
+            return node
+        }
+        if (position > 1) {
+            let node = new CreateNode(val)
+            this.length++;
+            node.next = previousNode
+            prevNode.next = node
+            return node
+        } else {
+            let node = new CreateNode(val)
+            this.start = node
+            node.next = previousNode
+
+        }
 
 
 
-//     print() {
-//         let current = this.start;
-//         while (current) {
-//             console.log(current.value);
-//             current = current.next;
-//         }
-//     }
+    }
 
-// }
 
-// const linkedList = new LinkedList();
+
+    print() {
+        let current = this.start;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+
+}
+
+const linkedList = new LinkedList();
 
 // linkedList.insert(1);
 // linkedList.insert(2);
 // linkedList.insert(3);
 // linkedList.insert(4);
 // linkedList.insert(5);
-// linkedList.insertPosition(44, 2)
+// linkedList.insertPosition(44, 1)
 // linkedList.insert(6);
 // linkedList.insert(7);
 // linkedList.print()
+debugger
+class DoubleCreateNode extends CreateNode {
+    constructor(value) {
+        super(value)
+        this.prev = null
+
+    }
+
+}
+
+class DoubleLinkList extends LinkedList {
+    constructor() {
+        super()
+    }
+    insert(value) {
+        this.length++
+            let node = new DoubleCreateNode(value)
+        if (this.start === null) {
+            this.start = node
+            return node
+        }
+        let current = this.start;
+
+        while (current.next) {
+
+            current = current.next
+        }
+        current.next = node
+        node.prev = current
+
+        return node
+    }
+
+    insertPosition(val, position) {
+        let previousNode = null;
+        let prevNode = null;
+        let current = this.start
+
+        if (this.start === null) {
+            let node = new DoubleCreateNode(val)
+            this.start = node
+            return node
+        }
+
+        for (let i = 0; i < position; i++) {
+            if (i > 0) {
+                prevNode = previousNode
+            }
+
+            previousNode = current
+            current = current.next
+
+
+
+        }
+
+
+        if (position > 1) {
+            let node = new CreateNode(val)
+            this.length++;
+
+            node.next = previousNode
+            prevNode.next = node
+            node.prev = prevNode
+            return node
+        } else {
+            let node = new CreateNode(val)
+            this.start = node
+            node.next = previousNode
+
+        }
+
+
+
+    }
+
+}
+
+
+let abc = new DoubleLinkList()
+abc.insert(1)
+abc.insert(2)
+abc.insert(3)
+abc.insert(4)
+abc.insert(5)
+abc.insert(6)
+abc.insertPosition(44, 1)
+abc.insertPosition(45, 5)
+abc.insertPosition(46, 3)
+abc.insertPosition(47, 7)
+
+abc.print()
