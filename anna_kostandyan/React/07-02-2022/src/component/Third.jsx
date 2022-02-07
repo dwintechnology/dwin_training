6116b5e789e2d9424318ca3eecd9abe1b0993d3e
Annak01 <anna.kostandyan.dwin@gmail.com>
2022-02-07 21:52:36 +0400

    Beginning

---
+++ anna_kostandyan/React/07-02-2022/src/component/Third.jsx
@@ -0,0 +1,10 @@
+function Third (){
+    return(
+        <>
+        <button onClick={(meow)=>{alert("Button1")}}>Button1</button>
+        <button onClick={(meow)=>{alert("Button2")}}>Button2</button>
+        <button onClick={(meow)=>{alert("Button3")}}>Button3</button>
+        </>
+    )
+}
+export default Third;
