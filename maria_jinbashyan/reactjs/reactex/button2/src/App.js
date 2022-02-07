70fe6245b0e72244844ba066be54dd8936102149
Maria200011 <maria.jinbashyan.dwin@gmail.com>
2022-02-08 01:06:22 +0400

    3Button input

---
+++ maria_jinbashyan/reactjs/reactex/button2/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Comp from './Comp';
+
+function App() {
+  return (
+    <div >
+    <Comp name="button 1"/>
+    <Comp name="button 2"/>
+    <Comp name="button 3"/>
+    </div>
+  );
+}
+
+export default App;
