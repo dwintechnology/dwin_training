fc04929e5a829394162eb80bbec720a3f6b91c73
Annak01 <anna.kostandyan.dwin@gmail.com>
2022-02-07 10:53:57 +0400

    Homework has changed

--- anna_kostandyan/React/03.02.2022/src/App.js
+++ anna_kostandyan/React/03.02.2022/src/App.js
@@ -6,17 +6,19 @@ import Text from './Component/Text';
 import Text1 from './Component/Text1';
 import Meow from './Component/Meow';
 import Errors from './Component/Errors';
-
+import Home from './Component/Home';
 function App() {
 return (
 <>
 <div style={{width:"100px", margin:"auto"}}>
-<a href='/post' style={{display:"block", marginBottom:"20px"}}>Home</a>
+<a href='/' style={{display:"block", marginBottom:"20px"}}>Home</a>    
+<a href='/post' style={{display:"block", marginBottom:"20px"}}>Post</a>
 <a href='/text' style={{display:"block", marginBottom:"20px"}} >K.W.</a>
 <a href='/text1' style={{display:"block", marginTop:"5px"}}>K.K.</a>
 </div>
 <Routes>
 <Route path="/post" element={<Post/>}/>
+<Route path="/" element={<Home/>}/>
 <Route path="/text" element={<Text/>}/>
 <Route path="/text1" element={<Text1/>}/>
 <Route path="post/:id" element={<Meow/>}/>
---
+++ anna_kostandyan/React/03.02.2022/src/Component/Home.jsx
@@ -0,0 +1,6 @@
+function Home(){
+    return(
+        <h1 style={{textAlign:"center"}}>Voxjuyn boloriiin!!!!</h1>
+    )
+}
+export default Home
