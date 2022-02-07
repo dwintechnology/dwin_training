6d5b76c3fcea0ec327a4bf3ececd8539ba07a334
Maria200011 <maria.jinbashyan.dwin@gmail.com>
2022-02-08 01:11:56 +0400

    map

---
+++ maria_jinbashyan/reactjs/reactex/map2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Map from './Map';
+
+function App() {
+  return (
+    <div >
+   <Map name="dog cat chicken cow sheep horse"/>
+    </div>
+  );
+}
+
+export default App;
