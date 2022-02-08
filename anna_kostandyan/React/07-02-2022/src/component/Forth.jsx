3038b10c1347c75e0f35b795a740a3f381fe3bae
Annak01 <anna.kostandyan.dwin@gmail.com>
2022-02-08 11:19:53 +0400

    Beginning

--- anna_kostandyan/React/07-02-2022/src/component/Forth.jsx
+++ anna_kostandyan/React/07-02-2022/src/component/Forth.jsx
@@ -1,15 +1,30 @@
 import { useState, useEffect } from "react"
-function Forth() {
-    let [count, setCount]=useState(0);
-    useEffect(()=>{
-        setInterval(()=>{
-            setCount((count)=>{return count +1})
-        },1000)
-    },[])
-    return(
-        <h1>{count}seconds</h1>
-    )}
-export default Forth;
+function Fourth(){
+    const [count, setCount] = useState(0);
+
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+    )
+}
+
+
+
+// function Forth() {
+//     let [count, setCount]=useState(0);
+//     useEffect(()=>{
+//         setInterval(()=>{
+//             setCount((count)=>{return count +1})
+//         },1000)
+//     },[])
+//     return(
+//         <h1>{count}seconds</h1>
+//     )}
+export default Fourth;
 
 
 
