474c89b6aa948ad1efe2289b8f94f94a201a8aad
Varuj2002 <varuzhan.hovsepyan.dwin@gmail.com>
2022-02-10 01:03:00 +0400

    Homework 09_02_2022 task 5,6,7

--- varuj_hovsepyan/react/09_02_2022/src/App.js
+++ varuj_hovsepyan/react/09_02_2022/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Checked from './components/Checked';
 import Slide from './components/Slide';
+import Login from './components/Login';
 
 function App() {
   return(
     <>
     <Checked/>
     <Slide/>
+    <Login/>
     </>
   )
 }
